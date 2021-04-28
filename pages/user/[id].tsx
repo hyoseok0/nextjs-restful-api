0c82ef6f7e7de32b9ebf8afacd056f181cd16855
hyoseok.choi <hyoseok.choi@navercorp.com>
2021-04-28 16:21:15 +0900

    Apply prettier & eslint

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -5,10 +5,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function User() {
   const router = useRouter()
-  const { data, error } = useSwr(
-    router.query.id ? `/api/user/${router.query.id}` : null,
-    fetcher
-  )
+  const { data, error } = useSwr(router.query.id ? `/api/user/${router.query.id}` : null, fetcher)
 
   if (error) return <div>Failed to load user</div>
   if (!data) return <div>Loading...</div>
